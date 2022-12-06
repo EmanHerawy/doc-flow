3c3d52b8cf8952912c80c1d14f2beb26a38ded32
emanherawy <eman.herawy@gmail.com>
2022-12-06 07:22:35 +0200

    add initail logic

--- src/Flow.sol
+++ src/Flow.sol
@@ -2,78 +2,81 @@
 
 pragma solidity 0.8.9;
 
-import { IAxelarGateway } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol';
-import { IAxelarGasService } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol';
-import { IERC20CrossChain } from './IERC20CrossChain.sol';
-import { ERC20 } from '@axelar-network/axelar-cgp-solidity/contracts/ERC20.sol';
-import { AxelarExecutable } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/executables/AxelarExecutable.sol';
-import { Upgradable } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/upgradables/Upgradable.sol';
-import { StringToAddress, AddressToString } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/StringAddressUtils.sol';
+import { IAxelarGateway } from '../lib/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol';
+import { IAxelarGasService } from '../lib/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol';
+import { AxelarExecutable } from '../lib/axelar-gmp-sdk-solidity/contracts/executables/AxelarExecutable.sol';
+// import { Upgradable } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/upgradables/Upgradable.sol';
+import { StringToAddress, AddressToString } from '../lib/axelar-gmp-sdk-solidity/contracts/StringAddressUtils.sol';
 
-contract ERC20CrossChain is AxelarExecutable, ERC20, Upgradable, IERC20CrossChain {
+contract ERC20CrossChain is AxelarExecutable {
     using StringToAddress for string;
     using AddressToString for address;
 
     error AlreadyInitialized();
 
+    event NextStep(string CurrentStep, string NextStep,address caller);
     event FalseSender(string sourceChain, string sourceAddress);
 
-    IAxelarGasService public immutable gasReceiver;
+// curretn step 
+bytes32 currentStep;
+bytes32 lastStep;
 
+// link each step to the next 
+// what if we have forked step ? need to rethink about it
+mapping (bytes32=>bytes32) stepTree;
+mapping (bytes32=>address) stepToAuthor;
+// owner 
+
+// steps with payment 
+mapping (bytes32=>uint) stepPayment;
+
+    IAxelarGasService public immutable gasReceiver;
+modifier onlyAuthor() {
+   
+   require(stepToAuthor[stepTree[currentStep]]== msg.sender,"caller is not assigned to this step");
+    _;
+}
     constructor(
         address gateway_,
         address gasReceiver_,
-        uint8 decimals_
-    ) AxelarExecutable(gateway_) ERC20('', '', decimals_) {
+       bytes32 [] memory steps,
+       address [] memory stepAuthors,
+       bytes32 [] memory crossChainSteps,
+       uint[] memory crossChainStepAmount
+    ) AxelarExecutable(gateway_) {
         gasReceiver = IAxelarGasService(gasReceiver_);
-    }
-
-    function _setup(bytes calldata params) internal override {
-        (string memory name_, string memory symbol_) = abi.decode(params, (string, string));
-        if (bytes(name).length != 0) revert AlreadyInitialized();
-        name = name_;
-        symbol = symbol_;
-    }
-
-    // This is for testing.
-    function giveMe(uint256 amount) external {
-        _mint(msg.sender, amount);
-    }
-
-    function transferRemote(
-        string calldata destinationChain,
-        address destinationAddress,
-        uint256 amount
-    ) public payable override {
-        _burn(msg.sender, amount);
-        bytes memory payload = abi.encode(destinationAddress, amount);
-        string memory stringAddress = address(this).toString();
-        if (msg.value > 0) {
-            gasReceiver.payNativeGasForContractCall{ value: msg.value }(
-                address(this),
-                destinationChain,
-                stringAddress,
-                payload,
-                msg.sender
-            );
+        // should check for arrays length
+        for (uint256 index = 0; index < steps.length-1; index++) {
+            stepTree[steps[index]]=steps[index+1];
+            stepToAuthor[steps[index]]=stepAuthors[index];
         }
-        gateway.callContract(destinationChain, stringAddress, payload);
-    }
-
-    function _execute(
-        string calldata, /*sourceChain*/
-        string calldata sourceAddress,
-        bytes calldata payload
-    ) internal override {
-        if (sourceAddress.toAddress() != address(this)) {
-            emit FalseSender(sourceAddress, sourceAddress);
-            return;
+        for (uint256 index = 0; index < crossChainSteps.length; index++) {
+            stepPayment[crossChainSteps[index]]=crossChainStepAmount[index];
         }
-        (address to, uint256 amount) = abi.decode(payload, (address, uint256));
-        _mint(to, amount);
+        lastStep = steps[steps.length];
     }
+function goNextStep() public onlyAuthor{
+ bool isCrossChain = stepPayment[stepTree[currentStep]]>0;
+  currentStep = stepTree[currentStep];
+    if (isCrossChain){
+        // do cross chain
+    }
+}
 
-    function contractId() external pure returns (bytes32) {
-        return keccak256('example');
-    }
+
+
+    // function _execute(
+    //     string calldata, /*sourceChain*/
+    //     string calldata sourceAddress,
+    //     bytes calldata payload
+    // ) internal override {
+    //     if (sourceAddress.toAddress() != address(this)) {
+    //         emit FalseSender(sourceAddress, sourceAddress);
+    //         return;
+    //     }
+    //     (address to, uint256 amount) = abi.decode(payload, (address, uint256));
+    //     _mint(to, amount);
+    // }
+
+ 
 }
